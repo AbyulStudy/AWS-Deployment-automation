8f3f5561441a2be0258ce19850282f618c448aaf
Abyul <kinsng1599@gmail.com>
2021-12-16 20:55:05 +0900

    feat: connection AWS RDS[mysql]

--- app.js
+++ app.js
@@ -1,11 +1,33 @@
+require("dotenv").config();
 const express = require('express');
 const app = express();
 const port = 80;
+const mysql = require('mysql2');
 
+// mysql Connection
+const connection = mysql.createConnection({
+    host: `${process.env.DATABASE_HOST}`,
+    user: `${process.env.DATABASE_USER}`,
+    password: `${process.env.DATABASE_PASSWORD}`,
+    port: `${process.env.DATABASE_PORT}`,
+    database: `${process.env.DATABASE_DATABASE}`
+});
+
+// console.log(process.env.DATABASE_HOST)
 app.get('/', (req,res) => {
     const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
 
     console.log('[GET]',ip)
+
+    connection.query(
+        'SELECT * FROM `BOARD_QA`',
+        function (err,res,fields){
+            console.error(err);
+            console.log(res);
+            console.log(fields);
+        }
+    )
+    
     res.send('hello world');
 })
 
