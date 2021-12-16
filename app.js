f3f79bbc079e3417933dfdf65d181f4cf0b4441c
Abyul <kinsng1599@gmail.com>
2021-12-16 14:30:15 +0900

    hotpix:port 8000 -> 80

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
-const port = 8000;
+const port = 80;
 
 app.get('/', (req,res) => {
     res.send('hello world');
 })
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`Example app listening at ${port}`)
 })
