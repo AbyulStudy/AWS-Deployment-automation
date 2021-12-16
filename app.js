3e29b2edb8aefb340d6f382367e7f48b11b9690d
Abyul <kinsng1599@gmail.com>
2021-12-16 15:08:19 +0900

    feat: [GET] console.log ADD

--- app.js
+++ app.js
@@ -3,9 +3,10 @@ const app = express();
 const port = 80;
 
 app.get('/', (req,res) => {
+    console.log("[GET]http://ec2-3-35-24-147.ap-northeast-2.compute.amazonaws.com/")
     res.send('hello world');
 })
 
 app.listen(port, () => {
-    console.log(`Example app listening at ${port}`)
+    console.log(`Example app listening at http://ec2-3-35-24-147.ap-northeast-2.compute.amazonaws.com/:${port}`)
 })
